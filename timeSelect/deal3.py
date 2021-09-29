892e94b2f7a8af2b7ea19b714e9e4ad85fefdfca
jeffery36 <jeffery6702@yahoo.com.tw>
2021-09-29 13:56:14 +0800

    feat: timeSelect 5 月

--- timeSelect/deal3.py
+++ timeSelect/deal3.py
@@ -9,12 +9,22 @@ az = df.get_group('AZ').reset_index().drop(['index'], axis = 1)
 bnt = df.get_group('bnt').reset_index().drop(['index'], axis = 1)
 mcv = df.get_group('高端').reset_index().drop(['index'], axis = 1)
 
-az_con = az[(az['contents'] == ' 綠共先打') & (az['id'] == 'kilhi')].index.tolist()[0]
-az = az.iloc[:az_con + 1, :]
+az_con3 = az[(az['contents'] == ' 綠共先打') & (az['id'] == 'kilhi')].index.tolist()[0]
+az3 = az.iloc[:az_con3 + 1, :]
 
-bnt_con = bnt[(bnt['contents'].str.contains('真的氣到快哭耶 Q_______Q')) & (bnt['id'] == 'jay0215')].index.tolist()[0]
-bnt = bnt.iloc[:bnt_con + 1, :]
+bnt_con3 = bnt[(bnt['contents'].str.contains('真的氣到快哭耶 Q_______Q')) & (bnt['id'] == 'jay0215')].index.tolist()[0]
+bnt3 = bnt.iloc[:bnt_con3 + 1, :]
 
-az.to_csv('./output/az.csv', index = False, encoding = 'utf-8-sig')
-bnt.to_csv('./output/bnt.csv', index = False, encoding = 'utf-8-sig')
+az3.to_csv('./output/az.csv', index = False, encoding = 'utf-8-sig')
+bnt3.to_csv('./output/bnt.csv', index = False, encoding = 'utf-8-sig')
 mcv.to_csv('./output/高端.csv', index = False, encoding = 'utf-8-sig')
+
+az_con5 = az[(az['contents'].str.contains('三樓  總統要打國產疫苗')) & (az['id'] == 'demitri')].index.tolist()[0]
+az5 = az.iloc[:az_con5 + 1, :]
+
+bnt_con5 = bnt[(bnt['contents'].str.contains('防護衣原料...那些原料')) & (bnt['id'] == 'awayaway')].index.tolist()[0]
+bnt5 = bnt.iloc[:bnt_con5 + 1, :]
+
+az5.to_csv('./output2/az.csv', index = False, encoding = 'utf-8-sig')
+bnt5.to_csv('./output2/bnt.csv', index = False, encoding = 'utf-8-sig')
+mcv.to_csv('./output2/高端.csv', index = False, encoding = 'utf-8-sig')

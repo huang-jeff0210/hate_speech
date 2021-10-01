3645fd3ccdccc8f475309b6c162a4b2e70e7e878
jeffery36 <jeffery6702@yahoo.com.tw>
2021-10-02 00:13:50 +0800

    輝瑞

--- methods/the_same_person.py
+++ methods/the_same_person.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('../data/康希諾.csv', names = ['href', 'id', 'contents', 'key', 'date'], error_bad_lines = False)
+df = pd.read_csv('../data/高端.csv', names = ['href', 'id', 'contents', 'key', 'date'], error_bad_lines = False)
 
 df['contents'] = df['contents'].astype('str')
 
@@ -23,7 +23,7 @@ for index, row in df.iterrows():
 df_fix['contents_order'] = df_fix['contents'].groupby(df_fix['href']).transform(lambda x: range(1, len(x)+1))
 print(df_fix['contents_order'])
 
-df_fix.to_csv('../data_fix/康希諾_fix.csv', index = False, encoding = 'utf-8-sig')
+df_fix.to_csv('../data_fix/高端_fix.csv', index = False, encoding = 'utf-8-sig')
 
 
 

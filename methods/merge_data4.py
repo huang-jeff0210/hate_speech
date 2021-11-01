58026ca9e656b10628cfd9aaf5e36e62edf4a6cc
DESKTOP-OTJVVE8\jeff0 <jeff006539@gmail.com>
2021-11-01 14:14:39 +0800

    feat hate.csv

--- methods/merge_data4.py
+++ methods/merge_data4.py
@@ -1,8 +1,10 @@
 import pandas as pd 
 
-df1 = pd.read_csv('./data_fix/疫苗相關4_1_fix.csv')
-df2 = pd.read_csv('./data_fix/疫苗相關4_2_fix.csv')
+df1 = pd.read_csv('output/標記1.csv')
+df2 = pd.read_csv('output/標記2.csv')
+df3 = pd.read_csv('output/標記3.csv')
 
-df1 = pd.concat([df1, df2], axis = 0)
+
+df1 = pd.concat([df1, df2, df3], axis = 0)
 
 df1.to_csv('./data_fix/疫苗相關4_fix.csv',index=False)
